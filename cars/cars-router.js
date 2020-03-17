a04bb8a4e035f5f9746a7d3ced9867711c5d7d01
Amanda Hogan <ahogan@teamworks.com>
2020-03-17 11:08:49 -0400

    Setup separate knex and db files

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,34 +1,59 @@
 const express = require('express');
-const knex = require('knex');
-
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/car-dealer.db3'
-  },
-  useNullAsDefault: true
-});
+const db = require('../data/db');
 
 const router = express.Router();
 
 router.post('/', validateCar, uniqueVIN, (req, res) => {
-
+  db('cars').insert(req.body)
+    .then(id => {
+      db('cars').where({ id: id[0] })
+        .then(newCar => {
+          res.status(201).json(newCar);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "The car information could not be retrieved" });
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The car could not be created" });
+    });
 });
 
 router.get('/', (req, res) => {
-
+  db('cars')
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The car information could not be retrieved" });
+    });
 });
 
 router.get('/:id', validateId, (req, res) => {
-
+  db('cars').where({ id: req.params.id }).first()
+    .then(car => {
+      res.status(200).json(car);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The car informatio could not be retrieved" });
+    });
 });
 
 router.delete('/:id', validateId, (req, res) => {
-
+  db('cars').where({ id: req.params.id }).first().del()
+    .then()
+    .catch(error => {
+      console.log(error):
+      res.status(500).json({ message: "The car could not be deleted" });
+    });
 });
 
 router.put('/:id', validateId, validateCar, uniqueVIN, (req, res) => {
-
+  db('cars').where({ id: req.params.id }).first()
 });
 
 function validateId(req, res, next) {
