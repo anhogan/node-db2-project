f6f35936acbe7d6bbc2c0ecdf885d25e2ff0aeac
Amanda Hogan <ahogan@teamworks.com>
2020-03-17 09:53:31 -0400

    Validate car fields and unique VIN middleware

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -11,7 +11,7 @@ const db = knex({
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateCar, uniqueVIN, (req, res) => {
 
 });
 
@@ -27,7 +27,7 @@ router.delete('/:id', validateId, (req, res) => {
 
 });
 
-router.put('/:id', validateId, (req, res) => {
+router.put('/:id', validateId, validateCar, uniqueVIN, (req, res) => {
 
 });
 
@@ -36,6 +36,34 @@ function validateId(req, res, next) {
     .then(car => {
       if (!car) {
         res.status(404).json({ message: "Invalid car id" });
+      } else {
+        next();
+      };
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The car information could not be retrieved" });
+    });
+};
+
+function validateCar(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Missing car data" });
+  } else if (!req.body.vin || !req.body.make || !req.body.model || !req.body.mileage) {
+    res.status(400).json({ message: "Each car must have a VIN, Make, Model, and Mileage listed" });
+  } else {
+    next();
+  };
+};
+
+function uniqueVIN(req, res, next) {
+  db('cars')
+    .then(cars => {
+      const taken = cars.filter((car) => {
+        return car.vin === req.body.vin
+      });
+      if (taken.length > 0) {
+        res.status(400).json({ message: "That VIN is already taken, please choose a unique value" });
       } else {
         next();
       };
